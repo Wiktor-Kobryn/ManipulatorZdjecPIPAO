b85d299e637b61e6fb089f3d20e38ef77b1c2d6f
KarolKolodziejczyk <kk303201@student.polsl.pl>
2023-12-12 20:57:22 +0100

    Zmiany z formatem pliku
    Zmiany z formatem pliku

--- ManipulatorZdjecPIPAO/mainwindow.cpp
+++ ManipulatorZdjecPIPAO/mainwindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent, Program *app)
     ui->setupUi(this);
 
     connect(this, SIGNAL(WybierzZdjecie()), app, SLOT(on_WybierzZdjecie()));
+    connect(this, SIGNAL(MirrorX()), app, SLOT(on_MirrorX()));
+    connect(this, SIGNAL(MirrorY()), app, SLOT(on_MirrorY()));
+
 }
 
 MainWindow::~MainWindow()
@@ -20,6 +23,39 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_BtnWybierzZdjecie_clicked()
 {
-    emit WybierzZdjecie();
+
+    QString imagePath = QFileDialog::getOpenFileName(this, "Wybierz obraz", "", "Obrazy (*.png)");
+    if (!imagePath.isEmpty()) {
+        QPixmap image(imagePath);
+        zdjecieRGB* ptr =  new zdjecieRGB(image);
+        this->mainApp->zdjecieObecne= ptr;
+
+        if (!image.isNull()) {
+          ui->LblObrazWe->setPixmap(image.scaled(ui->LblObrazWe->width(), ui->LblObrazWe->height(), Qt::KeepAspectRatio));
+          ui->LblSciezkaWe->setText(imagePath);
+
+        } else {
+          ui->LblObrazWe->setText("Nie można wczytać obrazu!");
+        }
+    }
+      emit WybierzZdjecie();
+}
+
+
+void MainWindow::on_BtnOdbicieOsiX_clicked()
+{
+    emit MirrorX();
+    if(this->mainApp->zdjecieObecne!=nullptr)
+        ui->LblObrazWe_2->setPixmap(this->mainApp->zdjecieObecne->topixmap().scaled(ui->LblObrazWe_2->width(),ui->LblObrazWe_2->height(), Qt::KeepAspectRatio));
+
+}
+
+
+void MainWindow::on_BtnOdbicieOsiY_clicked()
+{
+    emit MirrorY();
+    if(this->mainApp->zdjecieObecne!=nullptr)
+        ui->LblObrazWe_2->setPixmap(this->mainApp->zdjecieObecne->topixmap().scaled(ui->LblObrazWe_2->width(),ui->LblObrazWe_2->height(), Qt::KeepAspectRatio));
+
 }
 
