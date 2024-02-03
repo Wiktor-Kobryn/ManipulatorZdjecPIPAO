9234925c0d6ffdcb207ac7827f477cd21b29831c
wiktorkobryn <wk303200@student.polsl.pl>
2024-02-03 16:10:28 +0100

    dodanie klas pixela RGB i HSL
    klasy pixela RGB i HSL służą do przechowywania wartości kanałów i konwersji między nimi

--- ManipulatorZdjecPIPAO/transformacje.h
+++ ManipulatorZdjecPIPAO/transformacje.h
@@ -2,8 +2,50 @@
 #define TRANSFORMACJE_H
 
 #include "obraz.h"
+#include <algorithm>
 class Obraz;
 class ObrazRGB;
+class PixelHSL;
+class PixelRGB;
+class PixelCMYK;
+
+class PixelRGB
+{
+private:
+    int m_R = 0, m_G = 0, m_B = 0;
+
+public:
+    PixelRGB(int R, int G, int B);
+    PixelRGB() {}
+
+    int getR() {return m_R;}
+    int getG() {return m_G;}
+    int getB() {return m_B;}
+    void setR(int R);
+    void setG(int G);
+    void setB(int B);
+
+    PixelHSL konwertujDoHSL();
+};
+
+class PixelHSL
+{
+private:
+    int m_H = 0, m_S = 0, m_L = 0;
+
+public:
+    PixelHSL(int H, int S, int L);
+    PixelHSL() {}
+
+    int getH() {return m_H;}
+    int getS() {return m_S;}
+    int getL() {return m_L;}
+    void setH(int H);
+    void setS(int S);
+    void setL(int L);
+
+    PixelRGB konwertujDoRGB();
+};
 
 //<<abstract>>
 class TransfPix
@@ -27,7 +69,7 @@ private:
     int m_transfR = 0, m_transfG = 0, m_transfB = 0;
 
 public:
-    TransfPixRGB() :TransfPix() {};
+    TransfPixRGB() :TransfPix() {}
     TransfPixRGB(ObrazRGB* obraz, int transfR, int transfG, int transfB);
     TransfPixRGB(ObrazRGB* obraz);
 
@@ -45,6 +87,29 @@ public:
     void zapiszTransfKanaluB();
     void zerujTransformacje();
     void wpiszWartKanalu(std::byte* wartosc, int przesuniecie);
+};
+
+class TransfPixHSL : public TransfPix
+{
+private:
+    int m_transfH = 0, m_transfS = 0, m_transfL = 0;
+
+public:
+    TransfPixHSL() :TransfPix() {}
+    TransfPixHSL(ObrazRGB* obraz, int transfH, int transfS, int transfL);
+    TransfPixHSL(ObrazRGB* obraz);
+
+    int getTransfH() {return m_transfH;}
+    int getTransfS() {return m_transfS;}
+    int getTransfL() {return m_transfL;}
+    void setTransformacje(int transfH, int transfS, int transfL);
+    void setTransfH(int H) {m_transfH = H;}
+    void setTransfS(int S) {m_transfS = S;}
+    void setTransfL(int L) {m_transfL = L;}
+
+    void zapiszZmianeObrazu() override;
+    void zerujTransformacje();
+    //void wpiszWartKanalu(std::byte* wartosc, int przesuniecie);
 };
 
 #endif // TRANSFORMACJE_H
